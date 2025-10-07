4622f0c2ca7c5699fbb901e106ccd8a630c05ae0
Tomi126 <115153488+Tomi126@users.noreply.github.com>
2025-10-07 13:22:57 +0200

    backend

---
+++ fogado/backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'fogado',
+    port: 3307
+});
+
+app.get('/',(req, res) => {
+    res.send('Működik a szerver.');
+});
+
+app.get('/ping', async (req, res) => {
+    try {
+        await db.query('SELECT 1');
+        res.status(200).send('Database connection is OK');
+    } catch (error) {
+        console.error('Database connection error:', error);
+        res.status(500).send('Database connection failed');
+    }
+});
+
+
+app.get("/fogado", (req, res) => {  
+    const sql = "SELECT szobak.sznev AS 'Szobanév',szobak.agy AS 'Ágyak száma' FROM `szobak`;"; 
+    db.query(sql, (err, result) => {
+    if (err) return res.json(err); 
+    return res.json(result) 
+    })
+});
+
+app.get("/kihasznaltsag", (req, res) => {  
+    const sql = "SELECT szobak.sznev AS szobanev, COUNT(vendeg) AS vendegek, SUM(DATEDIFF(tav, erk)) AS vendegejszakak FROM foglalasok INNER JOIN szobak ON foglalasok.szoba = szobak.szazon GROUP BY szoba ORDER BY vendegejszakak ASC, vendegek ASC;"; 
+    db.query(sql, (err, result) => { 
+    if (err) return res.json(err); 
+    return res.json(result) 
+    })
+});
+
+app.get("/foglaltsag", (req, res) => { 
+    const sql = "SELECT vendegek.vnev AS 'Vendegnev',sznev AS 'Szobanev',foglalasok.erk AS 'Erkezes',foglalasok.tav AS 'Tavozas' FROM `szobak` INNER JOIN foglalasok ON szobak.szazon = foglalasok.szoba INNER JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz ORDER BY Vendegnev ASC;";
+    db.query(sql, (err, result) => {
+    if (err) return res.json(err); 
+    return res.json(result) 
+    })
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
