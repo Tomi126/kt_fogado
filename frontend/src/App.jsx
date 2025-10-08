4d69ee1e54bccc99de7b6b0d5d47981aaa2535ee
Tomi126 <115153488+Tomi126@users.noreply.github.com>
2025-10-08 15:04:08 +0200

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import './fogado.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-       <div>
-        <h1>Működik a frontend</h1>
-       </div>
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>Napraforgós Nemzeti Tanúsító Védjegy célja</h3>
+            <p>
+              A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma. A védjegy alapvető célja, hogy – összhangban az egyes szálláshelyek működtetéséről szóló 239/2009. Korm. rendeletben foglaltakkal – garanciát nyújtson a szálláshely szolgáltatás minőségének megfelelő színvonalára. A falusi vendégházak 1-4 napraforgós besorolást nyerhetnek el a külső, belső megjelenés, a felszereltség, a szolgáltatások színvonala, valamint a szállásadó személyes felkészültségének, szakmai képzettségének függvényében.
+            </p>
+          </div>
+          <div>
+            <h3>Falusi szálláshely fajtái</h3>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <h3>A hét törpe fogadó</h3>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>A vendégszobák foglalatsága</h3>
+            <div></div>
+          </div>
+          <div>
+            <h3>A szobák kihasználtsága:</h3>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
